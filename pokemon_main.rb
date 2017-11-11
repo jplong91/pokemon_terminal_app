0206c6fca0439c3b0c47af61560be594bd57ff0f
John Long <jplong91@gmail.com>
2017-11-11 12:50:04 -0600

    Beginning catch commands

--- pokemon_main.rb
+++ pokemon_main.rb
@@ -1,14 +1,36 @@
 puts "Welcome to the PokéApp"
 
-pokemon = ["Pikachu", "Bulbasaur", "Squirtle", "Charmander"]
+avl_pokemon = ["Pikachu", "Bulbasaur", "Squirtle", "Charmander"]
 
 while true
   puts "Type 'Catch' to start catching Pokémon, or 'Exit'."
-  input_option = gets.chomp
-  if input_option == "Catch"
+  input_main_option = gets.chomp
+  
+  if input_main_option == "Catch"
     puts "Let's Begin"
-    pokemon.shuffle!
-  elsif input_option == "Exit"
+    avl_pokemon.shuffle!
+    pokemon = avl_pokemon[0]
+    
+    puts "A wild #{pokemon} has appeared!!"
+
+    while true
+      catch_chance = rand(10)
+      puts "Type 'pkball' to throw Pokéball, 'feed' to make the Pokémon easier to catch! or 'run' to find another Pokémon."
+      input_catch_command = gets.chomp
+      if input_catch_command == "pkball"
+        puts "You throw a Pokéball at #{pokemon}"
+        p catch_chance
+      elsif input_catch_command == "feed"
+        puts "You sprinkle some food around #{pokemon}"
+      elsif input_catch_command == "run"
+        puts "You run away from the pokemon."
+        break
+      else
+        "Invalid option. Try again."
+      end
+    end
+
+  elsif input_main_option == "Exit"
     puts "Hope to see you again soon!"
     break
   else
